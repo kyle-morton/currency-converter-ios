e2b45505813abe1c1f2a1ca0b8957bc7e5eb442f
Kyle Morton <kylemortondevelopment@gmail.com>
2022-08-08 22:03:55 -0500

    init

---
+++ CurrencyConverter/CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Kyle Morton on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
